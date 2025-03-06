e506637319bde04500f18480d86fa77f7bc8702c
Michal Janusz <275463@student.pwr.edu.pl>
2025-03-06 16:11:42 +0100

    klasa odosobniona

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        int limit = 0;
+        public FizzBuzz()
+        {
+            limit = int.Parse(Console.ReadLine());
+        }
+
+        public void function()
+        {
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,37 +10,5 @@ namespace FizzBuzz
             fizzBuzz.function();
         }
     }
-    public class FizzBuzz
-    {
-        int limit = 0;
-        public FizzBuzz()
-        {
-            limit = int.Parse(Console.ReadLine());
-        }
-
-        public void function()
-        {
-            for (int i = 1; i <= limit; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-
-            }
-
-        }
-    }
+   
 }
